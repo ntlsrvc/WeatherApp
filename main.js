99b3d67ca701bef9cf8d78c5436bbd1ce7c6026c
Cortez <natalia1cortez@gmail.com>
2023-11-27 21:31:54 -0400

    feat: :sparkles: feat: Add weather details to the UI
    This commit adds weather details, including temperature, humidity, and wind speed, to the user interface. It also updates the CSS to style the weather section properly. The changes enhance the user experience by providing real-time weather information for the searched city. The API key and URL for weather data retrieval are also added. This feature improves the usability and functionality of the application.  Issue #12

---
+++ main.js
@@ -0,0 +1,17 @@
+const apiKey = "eb27d9e0892ccec2caf47ef56195f47d";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+async function checkWeather(city) {
+    const response = await fetch(apiUrl + ciity + `&appid=${apiKey}`);
+
+    var data = await response.json();
+
+    console.log(data);
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+}
+
+checkWeather();
